dc0e2398681d480be0dd3a17b5f9eea69a4632f3
hevinkalathiya <hevinvnsgu@gmail.com>
2025-01-03 14:49:14 +0530

    Enhance ShowLanguage component with loading state and improved UI; replace loading text with spinner and adjust layout

--- src/components/extension/ShowLanguage.tsx
+++ src/components/extension/ShowLanguage.tsx
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Loader2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { useGetLanguages } from "../../queries";
+import LeftArrowIcon from "../../assets/icons/LeftArrowIcon";
 
 const ShowLanguage = ({ setShowLanguagesData }: any) => {
   const { data: languages = [], isLoading } = useGetLanguages();
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [selectedLanguage, setSelectedLanguage] = useState(
-    languages[0]?.name
-  );
-
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]?.name);
 
   useEffect(() => {
     if (languages.length > 0) {
@@ -17,65 +15,70 @@ const ShowLanguage = ({ setShowLanguagesData }: any) => {
     }
   }, [selectedIndex, languages]);
 
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="p-4">
-      <div className="relative flex items-center justify-center">
+      <div className="relative flex items-center justify-center mb-8">
         <div className="rounded-full absolute left-0">
           <div
             onClick={() => setShowLanguagesData("")}
-            className="bg-plain rounded-full p-1 cursor-pointer"
+            className="bg-plain rounded-full p-0.5 cursor-pointer"
           >
-            <ArrowLeft className="h-5 w-5 text-subheading" />
+            <LeftArrowIcon />
           </div>
         </div>
         <h2 className="text-base font-semibold">Input Language</h2>
       </div>
 
-      <div className="p-2" tabIndex={0}>
-        {languages.map((language, index) => (
-          <div
-            key={language.id}
-            className={`p-4 cursor-pointer flex items-center justify-between rounded-full ${selectedIndex === index ? "bg-[#F8F8F8]" : "hover:bg-gray-50"
-              }`}
-            onClick={() => {
-              setSelectedIndex(index);
-              setSelectedLanguage(language);
-            }}
-          >
-            <span className="text-base">{language.name}</span>
-            {selectedLanguage?.id === language.id && (
-              <svg
-                className="w-5 h-5 text-red-500"
-                fill="none"
-                strokeWidth="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
+      {isLoading ? (
+        <div className="h-[450px] flex items-center justify-center">
+          <Loader2 size={32} className="animate-spin" />
+        </div>
+      ) : (
+        <>
+          <div className="" tabIndex={0}>
+            {languages.map((language, index) => (
+              <div
+                key={language.id}
+                className={`p-2.5 cursor-pointer flex items-center justify-between rounded-full ${
+                  selectedIndex === index ? "bg-[#F8F8F8]" : "hover:bg-gray-50"
+                }`}
+                onClick={() => {
+                  setSelectedIndex(index);
+                  setSelectedLanguage(language);
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-            )}
+                <span className="text-base text-foreground font-medium leading-5">
+                  {language.name}
+                </span>
+                {selectedLanguage?.id === language.id && (
+                  <svg
+                    className="w-5 h-5 text-red-500"
+                    fill="none"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <div className="p-4">
-        <Button
-          onClick={() => setShowLanguagesData(selectedLanguage)}
-          variant={"secondary"}
-          className="w-full rounded-full py-6"
-        >
-          Save
-        </Button>
-      </div>
+            <Button
+              onClick={() => setShowLanguagesData(selectedLanguage)}
+              variant={"secondary"}
+              size="lg"
+              className="w-full mt-4"
+            >
+              Save
+            </Button>
+        </>
+      )}
     </div>
   );
 };
